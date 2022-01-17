ddf2e623f8ca03cfe8a43c7b19277a3ebdcc819d
Gervasio Marchand <github@gervas.io>
2022-01-16 21:23:03 -0300

    heh, VERSION is set by the package

--- build.ps1
+++ build.ps1
@@ -1,6 +1,3 @@
 Import-Module BuildScripts
-Build-BSDotNetPackage -ProjectName AmeliaRSVP.Web -DockerImage mcr.microsoft.com/dotnet/sdk:6.0.101-alpine3.14
 
-if ($env:GITHUB_ENV) {
-    Write-Output "VERSION=$newVersion" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-}
+Build-BSDotNetPackage -ProjectName AmeliaRSVP.Web -DockerImage mcr.microsoft.com/dotnet/sdk:6.0.101-alpine3.14
